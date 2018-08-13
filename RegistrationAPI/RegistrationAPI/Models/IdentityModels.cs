5bdb89c8005dfa496f6bd973a3b865074fa65177
Khadija ALAdi <khadeee717@gmail.com>
2018-08-13 08:40:33 +0400

    adding c

---
+++ RegistrationAPI/RegistrationAPI/Models/IdentityModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace RegistrationAPI.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+}
